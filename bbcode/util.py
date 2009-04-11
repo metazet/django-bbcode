a17f36b651a7b24179b2ee73101cceb20fb91e3f
Mark Friedenbach <mark_friedenbach@yahoo.com>
2009-04-10 18:43:37 -0700

    converted comments to docstrings

--- bbcode/util.py
+++ bbcode/util.py
@@ -5,43 +5,65 @@
 # django-bbcode: util.py
 ##
 
-# Convert a string with BBCode markup into its corresponding HTML markup
-#
-# === Basic Usage
-#
-# The first parameter is the string off BBCode markup to be processed
-#
-#   text = "[b]some bold text to markup[/b]"
-#   output = BBRuby.to_html(text)
-#   # output => "<strong>some bold text to markup</strong>"
-#
-# === Custom BBCode translations
-#
-# You can supply your own BBCode markup translations to create your own custom markup
-# or override the default BBRuby translations (parameter is a hash of custom translations).
-#
-# The hash takes the following format: "name" => [regexp, replacement, description, example, enable_symbol]
-#
-#  custom_blockquote = {
-#    'Quote' => [
-#      /\[quote(:.*)?=(.*?)\](.*?)\[\/quote\1?\]/mi,
-#      '<div class="quote"><p><cite>\2</cite></p><blockquote>\3</blockquote></div>',
-#      'Quote with citation',
-#      '[quote=mike]please quote me[/quote]',
-#      :quote
-#    ]
-#  }
-#
-# === Enable and Disable specific tags
-#
-# BBRuby will allow you to only enable certain BBCode tags, or to explicitly disable certain tags.
-# Pass in either :disable or :enable to set your method, followed by the comma-separated list of tags
-# you wish to disable or enable
-#
-#   BBRuby.to_html(text, {}, true, :enable, :image, :bold, :quote)
-#   BBRuby.to_html(text, {}, true, :disable, :image, :video, :color)
-#
-def to_html(text, tags_alternative_definition = {}, escape_html=true, method=:disable, *tags)
+def bbcode2html(text,
+                tags_alternative_definition={},
+                escape_html=true,
+                method=:disable,
+                *tags):
+    """
+    Convert a string with BBCode markup into its corresponding HTML markup.
+
+    Basic Usage
+    -----------
+
+    The first parameter is the string off BBCode markup to be processed
+
+        text = "[b]some bold text to markup[/b]"
+        output = BBRuby.to_html(text)
+        # output => "<strong>some bold text to markup</strong>"
+
+    Custom BBCode translations
+    --------------------------
+
+    You can supply your own BBCode markup translations to create your own
+    custom markup, or override the default BBRuby translations (parameter is a
+    dictionary of custom translations).
+
+    The dictionary takes the following format:
+
+        "name": [regexp, replacement, description, example, enable_symbol]
+
+    For example,
+
+       custom_blockquote = {
+           'Quote': [
+               "\[quote(:.*)?=(.*?)\](.*?)\[\/quote\1?\]",
+               '<div class="quote"><p><cite>\2</cite></p><blockquote>\3</blockquote></div>',
+               'Quote with citation',
+               '[quote=mike]please quote me[/quote]',
+               'quote' ],
+       }
+
+    Enable and Disable specific tags
+    --------------------------------
+
+    Django-bbcode will allow you to only enable certain BBCode tags, or to
+    explicitly disable certain tags.  Pass in either 'disable' or 'enable' to
+    set your method, followed by the comma-separated list of tags you wish to
+    disable or enable.
+
+        ##
+        # Translate BBCode to HTML, enabling 'image', 'bold', and 'quote' tags
+        # *only*.
+        bbcode.util.bbcode2html(text, {}, true,
+                                'enable',  'image', 'bold',  'quote')
+
+        ##
+        # Translate BBCode to HTML, enabling all tags *except* 'image',
+        # 'bold', and 'quote'.
+        bbcode.util.bbcode2html(text, {}, true,
+                                'disable', 'image', 'video', 'color')
+    """
     text = text.clone
       
     # escape < and > to remove any html
